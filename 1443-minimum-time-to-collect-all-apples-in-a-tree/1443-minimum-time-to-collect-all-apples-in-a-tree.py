528315b5e2911cd5d67ac3fd9d7a30ecb16eff44
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-06-18 12:28:12 -0400

    Time: 550 ms (34.70%), Space: 55.6 MB (44.26%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+    # Time Complexity:O(n)
+    # More of a Graph problem
+    # mapping node to an empty arr initially
+        adj = {i:[] for i in range(n)}
+
+        for par, child in edges:
+            adj[par].append(child)
+            adj[child].append(par)
+
+        def dfs(cur, par):
+            time = 0 
+
+            for child in adj[cur]:
+                if child == par:
+                    continue 
+                childTime = dfs(child, cur)
+                if childTime or hasApple[child]:
+                    time += 2 + childTime
+            return time 
+        return dfs(0, -1)
+
+
+
