3dab75a9d9c19a6c3a894d5273f16234efc89102
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-07-25 10:04:02 -0400

    Time: 27 ms (98.37%), Space: 18.1 MB (30.33%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
@@ -14,10 +14,10 @@ class Solution:
         def helper(node, cur_max, cur_min):
             if not node:
                 return None 
-            
+            # This ensures that the function captures the maximum absolute difference between the current node and any of its ancestors.
             self.max_diff = max(self.max_diff, abs(cur_max - node.val),
                                 abs(cur_min - node.val))
-
+    
             
             cur_max = max(cur_max, node.val)
             cur_min = min(cur_min, node.val)
