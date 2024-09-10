0a45a9998d38e60040641794bd9f853066dbc184
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-10 09:53:41 -0400

    Time: 65 ms (48.75%), Space: 21 MB (27.66%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -8,19 +8,21 @@ class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         # time complexity: O(h) or O(logn) if it's a balanced tree
         res = [root.val]
-        
         def dfs(root):
             if not root:
-                return 0
-            
+                return 0 
+
             lmax = dfs(root.left)
             rmax = dfs(root.right)
             lmax = max(0, lmax)
             rmax = max(0, rmax)
-            # The first line checks if the current node's full path (both       children included) is the best path.
-            # The second line decides which one path (either left or right) should continue to the node's parent.
+
             res[0] = max(res[0], root.val + lmax + rmax)
-            return root.val + max(lmax,rmax)
-        
+            return root.val + max(lmax, rmax)
+
         dfs(root)
         return res[0]
+        
+
+        
+        
