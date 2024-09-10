ea855f331ab368fbd4a9ce32c4fc9a90a3b7b09d
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-10 08:57:10 -0400

    Time: 56 ms (50.62%), Space: 20.2 MB (81.76%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,21 +7,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # parent value 
         parent_val = root.val 
 
-        # Value of p 
-        p_val = p.val 
+        p_val = p.val
+        q_val = q.val
 
-        # Value of q 
-        q_val = q.val 
-
-        # If both p and q are greater than parent 
         if p_val > parent_val and q_val > parent_val:
-            return self.lowestCommonAncestor(root.right,p,q)
-        # If both p and q are lesser than parent 
-        elif p_val < parent_val and q_val< parent_val:
-            return self.lowestCommonAncestor(root.left,p,q)
-        # we have found the split point 
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p_val < parent_val and q_val < parent_val:
+            return self.lowestCommonAncestor(root.left, p, q)
         else:
-            return root 
+            return root
